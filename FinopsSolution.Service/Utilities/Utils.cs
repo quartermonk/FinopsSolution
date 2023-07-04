14a5e24ea64c658a12ad910abb7e62552f6293ae
quartermonk <132602892+quartermonk@users.noreply.github.com>
2023-07-04 12:16:43 +0530

    Update Utils.cs

--- FinopsSolution.Service/Utilities/Utils.cs
+++ FinopsSolution.Service/Utilities/Utils.cs
@@ -8,10 +8,10 @@ namespace FinopsSolution.Service.Utilities
 {
     public static class Utils
     {
-        public const string TenantId = "57594912-ac84-4ed8-be54-de05f5dc0433"; //Tenant ID Azure AD
-        public const string ClientId = "06734822-31ac-43dc-bfb7-ba6a08683e88"; //Application ID in Azure AD
-        public const string ClientSecret = "heq8Q~KY7CkKLno03I_vH2RKgwXwZVA8a3WNCc2m"; // App's secret Value (not Secret ID)
-        public const string subscriptionIdList = "79b7a5fc-a883-497d-ac63-7ef6b3de8bb0"; //Ex: xyz-xpto,abc-def
+        public const string TenantId = "fd4a983a-7713-4aae-8c12-629983f1fe91"; //Tenant ID Azure AD
+        public const string ClientId = "ebac428b-9843-4326-88d9-50f59048dd88"; //Application ID in Azure AD
+        public const string ClientSecret = "qmZ8Q~y4Q6Ai~zq3eFqP45T_9OB_~Dhizyfj6a0o"; // App's secret Value (not Secret ID)
+        public const string subscriptionIdList = "353c281f-ad57-43e1-8c7d-53f387cbec07"; //Ex: xyz-xpto,abc-def
 
         public const string DbConnectionString = "Server=(local)\\sqlexpress;Database=TransactionDB;Trusted_Connection=True;MultipleActiveResultSets=True;";
         public const int PercentageWarning = 10;
@@ -23,11 +23,11 @@ namespace FinopsSolution.Service.Utilities
         public const string EmailCredential = "email";
         public const string EmailPassword = "password";
 
-        public const string storageUri = "https://akshayfinopsdata.table.core.windows.net";
-        public const string accountName = "akshayfinopsdata";
-        public const string storageAccountKey = "ofRdvWqxqN1SyaVRSwBwHyXjUXWOKVurySMMA3ac4bnjxLKFPzcDb8WBzvxHMw4jePqY1155Ldxt+ASt/l/+zw==";
-        public const string StorageAccountConnStr = "DefaultEndpointsProtocol=https;AccountName=akshayfinopsdata;AccountKey=ofRdvWqxqN1SyaVRSwBwHyXjUXWOKVurySMMA3ac4bnjxLKFPzcDb8WBzvxHMw4jePqY1155Ldxt+ASt/l/+zw==;EndpointSuffix=core.windows.net";
-        public const int RunCostManagemementApiInSeconds = 300; //seconds to run the job
+        public const string storageUri = "https://storusefinopsdata.table.core.windows.net";
+        public const string accountName = "storusefinopsdata";
+        public const string storageAccountKey = "yaNYbwWY0KOPjP5yX0ssshiNBOT/2jPH0lNSYzEmH59lYAjpsgl7XbWYsQdeRuzI2UzRXu6AGQjU+AStXHslAg==";
+        public const string StorageAccountConnStr = "DefaultEndpointsProtocol=https;AccountName=storusefinopsdata;AccountKey=yaNYbwWY0KOPjP5yX0ssshiNBOT/2jPH0lNSYzEmH59lYAjpsgl7XbWYsQdeRuzI2UzRXu6AGQjU+AStXHslAg==;EndpointSuffix=core.windows.net";
+        public const int RunCostManagemementApiInSeconds = 3600; //seconds to run the job
         public const int RunEmailRescheduleInSeconds = 300; //seconds to run the job
     }
 }
